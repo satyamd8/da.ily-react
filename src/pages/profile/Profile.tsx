c1b5bb5dedfe7c08630a3c1eb2c283a32b8aa91b
satyamd8 <satyamdhar8@gmail.com>
2026-01-18 00:44:15 -0500

    updated unfinished pages to include a message

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,5 +1,20 @@
+import Navbar from "../../components/Navbar";
+import "./profile.css";
+
 const Profile = () => {
-   return <div>Profile</div>;
+   return (
+      <div className="profile-page">
+         <header>
+            <h3>da.ily</h3>
+            <Navbar />
+         </header>
+         <div className="wip">
+            <h1>Work In Progress</h1>
+            <br></br>
+            <h2>Check Back Later!</h2>
+         </div>
+      </div>
+   );
 };
 
 export default Profile;
--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -1,6 +1,19 @@
+import Navbar from "../../components/Navbar";
+import "./schedule.css";
+
 const Schedule = () => {
   return (
-    <div>Schedule</div>
+    <div className="schedule-page">
+      <header>
+        <h3>da.ily</h3>
+        <Navbar/>
+      </header>
+      <div className="wip">
+        <h1>Work In Progress</h1>
+        <br></br>
+        <h2>Check Back Later!</h2>
+      </div>
+    </div>
   )
 }
 
